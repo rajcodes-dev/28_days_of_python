e4c5c13d556e2dedbea184daeaca5d8919768aa0
rajcodes-dev <rajtiwari2006t@gmail.com>
2026-01-13 20:57:03 +0530

    complete program coin

--- day33_.py
+++ day33_.py
@@ -27,5 +27,10 @@ for experiment_number in range(10000):  # Run 100,000 experiments total.
     # Code that creates a list of 100 'heads' or 'tails' values
     rand_lst = [random.choice(["H", "T"]) for i in range(100)]
     # Code that checks if there is a streak of 6 heads or tails in a row
+    for i in range(100):
+        if rand_lst[i:i+6] == ['T','T','T','T','T','T'] or \
+        rand_lst[i:i+6] == ['H','H','H','H','H','H']:
+            number_of_streaks += 1
+            break
 
 print('Chance of streak: %s%%' % (number_of_streaks / 100))
